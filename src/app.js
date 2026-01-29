86c3f98551ba37bd30c669bb2e449415b5f79686
vishalchauhan2109 <vishalchauhan2109@gmail.com>
2026-01-29 14:35:55 +0530

    Cors error fixed

--- src/app.js
+++ src/app.js
@@ -2,21 +2,33 @@ const express = require("express");
 const DB = require("./Config/database");
 const app = express();
 const cookieparser = require("cookie-parser");
-const cors = require("cors")
+// const cors = require("cors")
 const path = require("path");
 
+// const cors = require("cors");
 const cors = require("cors");
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://devcirclefrontendd.onrender.com"
+];
 
 app.use(
   cors({
-    origin: [
-      "https://devcirclefrontendd.onrender.com",
-      "http://localhost:5173"
-    ],
+    origin(origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
-app.options("*", cors());
+
+
 
 app.use(express.json());
 app.use(cookieparser());
@@ -27,7 +39,7 @@ const profileRouter = require("./routes/profile")
 const requestRouter = require("./routes/request");
 const postRouter = require("./routes/post");
 
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -41,7 +53,7 @@ DB()
     console.log("database is connected");
 
     app.listen(2100, () => {
-      console.log("your server is created ");
+      console.log("your server is created ",`${2100}`);
     });
   })
   .catch((error) => {
