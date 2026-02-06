1a35a597bfa27cc32c4b1382c17d6d879a03f237
vishalchauhan2109 <vishalchauhan2109@gmail.com>
2026-02-06 17:00:40 +0530

    changed the frontend url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://iridescent-cassata-dcb65a.netlify.app"
+    "http://iridescent-cassata-dcb65a.netlify.app"
   ],
   credentials: true
 }));
